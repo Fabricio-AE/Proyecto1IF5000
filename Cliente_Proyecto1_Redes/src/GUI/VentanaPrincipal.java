26ae74f1cd1f9c00f35b3d1bb554b315d3d32bc6
Fabricio-A-E <fabricioae21@gmail.com>
2021-06-01 09:27:39 -0600

    Validaciones

--- Cliente_Proyecto1_Redes/src/GUI/VentanaPrincipal.java
+++ Cliente_Proyecto1_Redes/src/GUI/VentanaPrincipal.java
@@ -5,21 +5,27 @@
  */
 package GUI;
 
+import Client.ClientConnection;
 import Domain.Cliente;
+import Utility.Conversiones;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.HeadlessException;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import org.jdom.Element;
 
 /**
  *
  * @author Fabricio
  */
-public class VentanaPrincipal extends JFrame {
+public class VentanaPrincipal extends JFrame implements WindowListener {
 
     private JPanel panel, panelCliente, panelServidor;
 
@@ -30,8 +36,9 @@ public class VentanaPrincipal extends JFrame {
             this.setSize(new Dimension(800, 700));
             this.setLayout(null);
             this.setLocationRelativeTo(null);
-            this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
             this.setResizable(false);
+            this.addWindowListener(this);
             this.init();
             this.setVisible(true);
         } catch (IOException ex) {
@@ -54,6 +61,53 @@ public class VentanaPrincipal extends JFrame {
         catch (IOException ex) {
             ex.printStackTrace();
         }
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent we) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent we) {
+        String[] btns = {"Aceptar", "Cancelar"};
+        int opc = JOptionPane.showOptionDialog(this, "Desea salir?", "Mensaje", 0, 0, null, btns, this);
+        if (opc == JOptionPane.YES_OPTION) {
+            if (!ClientConnection.isNull()) {
+                try {
+                    ClientConnection cliConn = ClientConnection.getInstance();
+                    cliConn.enviar(Conversiones.anadirAccion(new Element("msg"), "Cerrar"));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }//try-catch
+            }
+            System.exit(0);
+        }//if
+    }
+
+    @Override
+    public void windowClosed(WindowEvent we) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent we) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent we) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent we) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent we) {
 
     }
 
