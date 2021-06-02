4bb23912833620391cdc16311e9b663709c677d8
Fabricio-A-E <fabricioae21@gmail.com>
2021-06-02 11:49:46 -0600

    ServidorSQL

--- Servidor_Proyecto1_Redes/src/Utility/Variables.java
+++ Servidor_Proyecto1_Redes/src/Utility/Variables.java
@@ -2,7 +2,7 @@ package Utility;
 
 public class Variables {
 	public static int PORTNUMBER=5025;
-	public static String SERVIDORSQL = "jdbc:sqlserver://DESKTOP-OI0NS7V\\SQLSERVERDEV.database.windows.net:1433;";
+	public static String SERVIDORSQL = "jdbc:sqlserver://localhost\\SQLSERVERDEV:1433;";
 	public static String BDSERVIDORSQL = "database=PROYECTO1_REDES;";
 	public static String USUARIOSERVIDORSQL = "user=sa;";
 	public static String PASSWORDSERVIDORSQL = "password=2112;";
